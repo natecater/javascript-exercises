29c036bcdbb7c6984a614af993230bb8623be62c
Nate Cater <ncater3@gatech.edu>
2022-11-25 16:27:25 -0800

    ex5 in progress, stuck on checking if args are numbers

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
 
+    if (start.isInteger() || end.isInteger()) {
+        
+    };
+
+    counter = end;
+    
+    if (start > end) {
+        counter = start;
+    }
+    
+    sum = 0;
+
+    while (counter >= start) {
+        sum += counter;
+        counter--;
+    }
+
+    return sum;
+    
 };
 
 // Do not edit below this line

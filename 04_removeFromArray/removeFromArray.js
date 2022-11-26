92e593df6acf3961130b64e532c915f57be0762e
Nate Cater <ncater3@gatech.edu>
2022-11-25 16:18:20 -0800

    pass exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...elements) {
 
+    result = [];
+    result = arr;
+
+    for (let element of elements) {
+        for (let i = 0; i < result.length; i++) {
+            if (element === result[i]) {
+                arr.splice(i, 1);
+            }
+        }
+    }
+
+    return result;
 };
 
 // Do not edit below this line

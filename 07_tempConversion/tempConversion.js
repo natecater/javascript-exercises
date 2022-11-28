ddbb15f021bd4dd55e84fb0c11f88987125fb2c3
Nate Cater <ncater3@gatech.edu>
2022-11-27 17:38:23 -0800

    pass ex7 tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,16 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahr) {
+
+  let cels = (5/9 * (fahr - 32));
+  let result = Math.round(cels * 10) / 10;
+  return result;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cels) {
+
+
+  let fahr = ((cels * 1.8) + 32);
+  let result = Math.round(fahr * 10) / 10;
+  return result;
 };
 
 // Do not edit below this line
